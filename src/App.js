9b99c1e0938efb236dc4ae211f402bc47a817143
manikandan <lovelymanil272@gmail.com>
2025-09-25 14:18:41 +0530

    comment

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Portfolio from './components/about';
 
-const Resume = () => {
-  const handleEmailClick = () => {
-    window.location.href = 'mailto:lovelymanil272@gmail.com';
-  };
 
-  const handleDownloadClick = () => {
-    const link = document.createElement('a');
-    link.href = '/resume.pdf'; // public folder la irukkanum
-    link.download = 'Manikandan_Resume.pdf';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-
+const App = () => {
   return (
-    <>
-      <nav>
-        <Link to="/">Manikandan</Link>
-        <Link to="/home">Home</Link>
-      </nav>
-
-      <section>
-        <h1>I'm Manikandan</h1>
-        <button onClick={handleEmailClick}>Email Me</button>
-        <button onClick={handleDownloadClick}>Download Resume</button>
-      </section>
-    </>
+    <div>
+      <Portfolio />
+    </div>
   );
 };
 
-export default Resume;
+export default App;
